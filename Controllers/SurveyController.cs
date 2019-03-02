98b0736a9a1112b40ea5003ed4f437ebe28b47af
@Pikhota <s.pikhota@gmail.com>
2019-03-02 19:45:35 +0200

    add controllers

---
+++ Controllers/SurveyController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SurveyWebPlatform.Models;
+
+namespace SurveyWebPlatform.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SurveyController : ControllerBase
+    {
+        private readonly SurveyContext _context;
+
+        public SurveyController(SurveyContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Survey>>> GetSurveys()
+        {
+            return await _context.Surveys.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Survey>> GetSurvey(int id)
+        {
+            var survey = await _context.Surveys.FindAsync(id);
+
+            if (survey == null)
+            {
+                return NotFound();
+            }
+
+            return survey;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSurvey(int id, Survey survey)
+        {
+            if (id != survey.SurveyId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(survey).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SurveyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Survey>> CreateSurvey(Survey survey)
+        {
+            _context.Surveys.Add(survey);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSurvey", new { id = survey.SurveyId }, survey);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Survey>> DeleteSurvey(int id)
+        {
+            var survey = await _context.Surveys.FindAsync(id);
+            if (survey == null)
+            {
+                return NotFound();
+            }
+
+            _context.Surveys.Remove(survey);
+            await _context.SaveChangesAsync();
+
+            return survey;
+        }
+
+        private bool SurveyExists(int id)
+        {
+            return _context.Surveys.Any(e => e.SurveyId == id);
+        }
+    }
+}
--- Models/SurveyContext.cs
+++ Models/SurveyContext.cs
@@ -4,9 +4,9 @@ namespace SurveyWebPlatform.Models
 {
 	public class SurveyContext : DbContext
 	{
-		DbSet<Survey> Surveys { get; set; }
-		DbSet<Question> Questions { get; set; }
-		DbSet<Variant> Variants { get; set; }
+		public DbSet<Survey> Surveys { get; set; }
+		public DbSet<Question> Questions { get; set; }
+		public DbSet<Variant> Variants { get; set; }
 		public SurveyContext(DbContextOptions<SurveyContext> options) : base(options)
 		{
 
